16978c7973c387eeaf4d13940acbe9ad7536b497
dusanCemovic <dusan.cemovic@gmail.com>
2026-03-25 19:39:39 +0100

    Step 7: Filament addition

---
+++ app/Filament/Resources/Reservations/Tables/ReservationsTable.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources\Reservations\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\Layout\Split;
+
+class ReservationsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->label('User')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('room.title')
+                    ->label('Room')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('date_from')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('date_to')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email address')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
